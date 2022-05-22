6feca8bf1bddef29b077e9da34ff67a5dbbed996
Konrad Münch <konrad.muench@student.htw-berlin.de>
2022-05-23 01:17:18 +0200

    pump controll to esp

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,15 @@
 #include <string.h>
 #include <SoftwareSerial.h>
 static PubSubClient connected_mqtt_client;
-SoftwareSerial linkSerial(13,15 ); // RX, TX
+SoftwareSerial linkSerial(13, 15); // RX, TX
 
-StaticJsonDocument<300> doc;
-char out[300];
+char out[128];
 
 void setup()
 {
     Serial.begin(115200);
+    while (!Serial)
+        continue;
     connectTohWifi();
     connected_mqtt_client = connectToBroker();
 
@@ -28,25 +29,17 @@ void loop()
 {
     if (linkSerial.available())
     {
+        StaticJsonDocument<300> doc;
         // Allocate the JSON document
         // This one must be bigger than for the sender because it must store the strings
 
         // Read the JSON document from the "link" serial port
+        Serial.setTimeout(5000);
         DeserializationError err = deserializeJson(doc, linkSerial);
 
         if (err == DeserializationError::Ok)
         {
-            // Print the values
-            // (we must use as<T>() to resolve the ambiguity)
-
-            Serial.print("moisture sensor 1 = ");
-            Serial.println(doc["moisture_sensor_1"].as<int>());
-            Serial.print("moisture sensor 2 = ");
-            Serial.println(doc["moisture_sensor_2"].as<int>());
-            Serial.print("water level = ");
-            Serial.println(doc["water_level"].as<float>());
-            Serial.print("pump state = ");
-            Serial.println(doc["pump_on"].as<int>());
+            Serial.println(out);
         }
         else
         {
@@ -58,9 +51,47 @@ void loop()
             while (linkSerial.available() > 0)
                 linkSerial.read();
         }
-    }
+        
+        serializeJson(doc, out);
 
-    serializeJson(doc, out);
-    connected_mqtt_client.publish("tele/watering/state", out);
-    delay(1000);
+        connected_mqtt_client.publish("tele/watering/state", out);
+        delay(1000);
+    }
 }
+
+  // float distance_cm = water_level_measurement(ULTRA_SONIC_TRIGGER_PIN, ULTRA_SONIC_ECHO_PIN);
+
+  // if (current_water_tank_level > WATER_TANK_LEVEL_MIN)
+  // {
+  //   info_text = "Water level low, refill!";
+  //   Serial.println(info_text);
+  //   pump_on = false;
+  //   stop_pump();
+  // }
+  // else if (current_water_tank_level < WATER_TANK_LEVEL_MAX)
+  // {
+  //   info_text = "Water level too high, drain!";
+  //   Serial.println(info_text);
+  // }
+  // else
+  // {
+
+  //   if (((moisture_value == SOIL_DRY) && (!pump_on)) || ((start_pump_cmd_extern == true)))
+  //   {
+  //     pump_on = true;
+  //     start_pump();
+  //     info_text = "Start pump";
+  //     start_pump_cmd_extern = false;
+  //   }
+  //   else if (((moisture_value == SOIL_WET) && (pump_on)) || ((stop_pump_cmd_extern == true)))
+  //   {
+  //     pump_on = false;
+  //     stop_pump();
+  //     info_text = "stop pump";
+  //     stop_pump_cmd_extern = false;
+  //   }
+  //   else
+  //   {
+  //     ;
+  //   }
+  // }
